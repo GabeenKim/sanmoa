b51d71cae66f14bfa6866880c2b4da9fe8d3711a
GabeenKim <rlarkqls5551250@gmail.com>
2022-09-01 14:15:58 +0900

    route

--- src/api/store.js
+++ src/api/store.js
@@ -1,43 +1,44 @@
-import { Router } from "express";
-import request from "request";
+import { Router } from 'express';
+import request from 'request';
 
 const router = Router();
 
 //주변관광정보
-router.get("/search", async (req, res) => {
+router.get('/search', async (req, res) => {
+  let regionCode = '2';
   var url =
-    "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList";
+    'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList';
   var queryParams =
-    "?" +
-    encodeURIComponent("serviceKey") +
-    "=e9q6FRryTM2vX8VQxrb8dJwbnlvpvHu447HuwfJQw0zl%2B7cnoIu6HdJElNMaGpaKQ3sQ2GAEsOad%2BOWNCwJ%2FVg%3D%3D"; /* Service Key*/
+    '?' +
+    encodeURIComponent('serviceKey') +
+    '=e9q6FRryTM2vX8VQxrb8dJwbnlvpvHu447HuwfJQw0zl%2B7cnoIu6HdJElNMaGpaKQ3sQ2GAEsOad%2BOWNCwJ%2FVg%3D%3D'; /* Service Key*/
   queryParams +=
-    "&" +
-    encodeURIComponent("numOfRows") +
-    "=" +
-    encodeURIComponent("10"); /* */
+    '&' +
+    encodeURIComponent('numOfRows') +
+    '=' +
+    encodeURIComponent('1000'); /* */
   queryParams +=
-    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+    '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
   queryParams +=
-    "&" +
-    encodeURIComponent("MobileOS") +
-    "=" +
-    encodeURIComponent("ETC"); /* */
+    '&' +
+    encodeURIComponent('MobileOS') +
+    '=' +
+    encodeURIComponent('ETC'); /* */
   queryParams +=
-    "&" +
-    encodeURIComponent("MobileApp") +
-    "=" +
-    encodeURIComponent("AppTest"); /* */
+    '&' +
+    encodeURIComponent('MobileApp') +
+    '=' +
+    encodeURIComponent('AppTest'); /* */
   queryParams +=
-    "&" +
-    encodeURIComponent("areaCode") +
-    "=" +
-    encodeURIComponent("1"); /*지역 */
+    '&' +
+    encodeURIComponent('areaCode') +
+    '=' +
+    encodeURIComponent(regionCode); /*지역 */
 
   request(
     {
       url: url + queryParams,
-      method: "GET",
+      method: 'GET',
     },
     function (error, response, body) {
       //console.log("Status", response.statusCode);
